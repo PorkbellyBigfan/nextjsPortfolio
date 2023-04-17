aeed758f1679056ec3bf26e27a454907ecb20d45
porkbellyweb <porkbellyweb@Gmail.com>
2023-04-17 23:38:30 +0900

    add darkmode

--- components/header.js
+++ components/header.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import DarkModeToggleBtn from './dark-mode-toggle-btn';
 
 export default function Header(){
   return(
@@ -24,11 +25,8 @@ export default function Header(){
               <a className="mr-5 hover:text-gray-900">About me</a>
             </Link>
           </nav>
-          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
-            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </button>
+          {/*다크모드 토글버튼 자리*/}
+          <DarkModeToggleBtn/>
         </div>
       </header>
     </>
